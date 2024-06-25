d4e4c1c41fb436e1c46c726ec9250db0d4aad5f1
MaarcusRenieroL <maarcusreniero.l@gmail.com>
2024-06-26 00:19:02 +0530

    init: init shadcn-ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
+import { Button } from "~/components/ui/button";
+
 export default function Home() {
   return (
-    <div className="h-full w-full flex items-center justify-center">
+    <div className="h-screen w-screen flex items-center justify-center">
       <h1 className="text-2xl font-bold">E commerce store</h1>
+      <Button>click me</Button>
     </div>
   );
 }
