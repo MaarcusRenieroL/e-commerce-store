823d14a64bcc45151fea408b31dbf36c90fcc238
MaarcusRenieroL <maarcusreniero.l@gmail.com>
2024-06-26 00:16:20 +0530

    init: init next app

---
+++ src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="h-full w-full flex items-center justify-center">
+      <h1 className="text-2xl font-bold">E commerce store</h1>
+    </div>
+  );
+}
